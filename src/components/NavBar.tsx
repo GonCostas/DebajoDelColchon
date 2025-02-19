22df781e579ec51c3746b5a74dbd783222639383
Costas Gonzalo <103608115+Raba-nito@users.noreply.github.com>
2025-02-19 18:23:29 -0300

    configurando vite-config

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,9 +8,9 @@ const NavBar: React.FC = () => {
       <div className="appName">DEBAJO DEL COLCHÓN</div>
 
       <div className="links">
-        <a href="/pageDolar" className="link">DOLAR</a>
-        <a href="/pageEuro" className="link">EURO</a>
-        <a href="/pageInflacion" className="link">INFLACION</a>
+        <a href="/DebajoDelColchon/Dolar" className="link">DOLAR</a>
+        <a href="/DebajoDelColchon/Euro" className="link">EURO</a>
+        <a href="/DebajoDelColchon/Inflacion" className="link">INFLACION</a>
       </div>
     </nav>
   );
