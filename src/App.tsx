785f0de1a498a1ee378ac0c8532e020b6374d296
Costas Gonzalo <103608115+Raba-nito@users.noreply.github.com>
2025-02-19 21:08:46 -0300

    fix rutas para github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import PageEuro from "./pages/PageEuro";
