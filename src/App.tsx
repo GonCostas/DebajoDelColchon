954ee177812ad492ca7689cb6fc79e02f16e3dc6
Costas Gonzalo <103608115+Raba-nito@users.noreply.github.com>
2025-02-19 21:28:24 -0300

    se pone BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import PageEuro from "./pages/PageEuro";
